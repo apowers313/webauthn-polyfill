f77d3af3654601ae6ea1bddf2107e87dc6e5668d
Adam Powers <apowers@ato.ms>
2015-09-29 22:52:51 -0700

    Completed normalize algorithm

--- fido2.js
+++ fido2.js
@@ -11,27 +11,134 @@ window.fido = (function () {
     var fidoAPI = function () {};
 
     function _makeRpId(origin) {
-        var parser = document.createElement('a');
+        var parser = document.createElement("a");
         parser.href = origin;
         // console.log("RPID:", parser.hostname);
         return parser.hostname;
     }
 
+    var supportedAlgorithms = {
+        "aes-cbc": {
+            name: "AES-CBC",
+            dict: {
+                name: "AES-CBC",
+            },
+            supports: ["encrypt", "decrypt", "generateKey"],
+        },
+        "aes-ctr": {
+            name: "AES-CTR",
+            dict: {
+                name: "AES-CTR",
+            },
+            supports: ["encrypt", "decrypt", "generateKey"],
+        },
+        "aes-gcm": {
+            name: "AES-GCM",
+            dict: {
+                name: "AES-GCM",
+            },
+            supports: ["encrypt", "decrypt", "generateKey"],
+        },
+        "pbkdf2": {
+            name: "PBKDF2",
+            dict: {
+                name: "PBKDF2",
+            },
+            supports: ["derive_key"],
+        },
+        "md2": {
+            name: "MD2",
+            dict: {
+                name: "MD2",
+            },
+            supports: ["digest"],
+        },
+        "md5": {
+            name: "MD5",
+            dict: {
+                name: "MD5",
+            },
+            supports: ["digest"],
+        },
+        "sha-1": {
+            name: "SHA-1",
+            dict: {
+                name: "SHA-1",
+            },
+            supports: ["digest"],
+        },
+        "sha-256": {
+            name: "SHA-256",
+            dict: {
+                name: "SHA-256",
+            },
+            supports: ["digest"],
+        },
+        "sha-384": {
+            name: "SHA-384",
+            dict: {
+                name: "SHA-384",
+            },
+            supports: ["digest"],
+        },
+        "sha-512": {
+            name: "SHA-512",
+            dict: {
+                name: "SHA-512",
+            },
+            supports: ["digest"],
+        },
+        "rsassa-pkcs1-v1_5": {
+            name: "RSASSA-PKCS1-v1_5",
+            dict: {
+                name: "RSASSA-PKCS1-v1_5",
+            },
+            supports: ["sign", "verify", "generateKey"],
+        },
+        "rsaes-pkcs1-v1_5": {
+            name: "RSAES-PKCS1-v1_5",
+            dict: {
+                name: "RSAES-PKCS1-v1_5",
+            },
+            supports: ["encrypt", "decrypt", "generateKey"],
+        }
+    };
+
     function _normalizeAlgorithm(keyAlgorithm) {
-        var subtle = window.crypto.subtle.__proto__;
-        var key, i;
-        var encrypt = subtle.encrypt;
-
-        console.log("Subtle is", subtle);
-        console.log("Encrypt is", encrypt);
-        console.log("Encrypt name", encrypt.name);
-
-        // for (key in Object.keys(subtle)) {
-        //     console.log ("Subtle key:", key);
-        // }
-        // for (i = 0; i < encrypt.length; i++) {
-        //     console.log ("Encrypt: ", encrypt[i].name);
-        // }
+        var res = {};
+        if (typeof keyAlgorithm === "string") {
+            keyAlgorithm = keyAlgorithm.toLowerCase();
+            if (keyAlgorithm in supportedAlgorithms) {
+                if ("dict" in supportedAlgorithms[keyAlgorithm]) {
+                    res = supportedAlgorithms[keyAlgorithm].dict;
+                    res = normalizeAlgorithm(res);
+                } else {
+                    throw new Error("Algorithm didn't have dictionary");
+                }
+            } else {
+                throw new Error("Unknown algorithm");
+            }
+        } else {
+            var key, val, tmp;
+            for (key in keyAlgorithm) {
+                if (key === "name") {
+                    val = keyAlgorithm[key];
+                    if (val in supportedAlgorithms) {
+                        tmp = supportedAlgorithms[val];
+                        tmp = tmp.dict.name;
+                    } else {
+                        tmp = val;
+                    }
+                    res[key] = tmp;
+                }
+                if (key !== "name" && keyAlgorithm[key] in supportedAlgorithms) {
+                    res[key] = normalizeAlgorithm(keyAlgorithm[key]);
+                } else {
+                    res[key] = keyAlgorithm[key];
+                }
+            }
+        }
+        return res;
     }
 
     /**
@@ -91,10 +198,16 @@ window.fido = (function () {
                 };
                 current = null;
                 // TODO: not quite sure how to make this work from userland...
-                // _normalizeAlgorithm (keyAlgorithm);
+                console.log ("keyAlgorithm", keyAlgorithm);
+                var x = _normalizeAlgorithm(keyAlgorithm);
+                console.log ("Normalize Algorithm returned:", x);
             }
+            // should be a valid AlgorithmIdentifier object
             cryptoParameters = current;
 
+            // TODO: process _extensionHookList
+
+            // attempt to make credentials on each authenticator
             var _pendingList = [];
             for (i = 0; i < _authenticatorList.length; i++) {
                 // Web API 4.1.1 says to call with: callerOrigin, rpId, account, current.type, normalizedAlgorithm, blacklist, attestationChallenge and clientExtensions
@@ -112,8 +225,8 @@ window.fido = (function () {
                 );
             }
 
-            // basically Promises.all() that doesn't die on failure
-            // TODO: this probably doesn't work if the timer lapses, since it won't .then() anything after the promise that hung
+            // basically Promises.all() that doesn"t die on failure
+            // TODO: this probably doesn"t work if the timer lapses, since it won"t .then() anything after the promise that hung
             // use some version of Promises.race() instead
             function resolveAll(promises) {
                 var accumulator = [];
@@ -137,12 +250,12 @@ window.fido = (function () {
             resolveAll(_pendingList)
                 .then(function (ret) {
                     // console.log("all promises resolved:", ret);
-                    window.clearTimeout (makeCredentialTimer);
-                    resolve (ret);
+                    window.clearTimeout(makeCredentialTimer);
+                    resolve(ret);
                 })
                 .catch(function (err) {
-                    console.log ("caught error");
-                    reject (err);
+                    console.log("caught error");
+                    reject(err);
                 });
         });
     };
@@ -279,14 +392,14 @@ window.fido = (function () {
         constructor: fidoAuthenticator,
         authenticatorDiscover: function () {},
         authenticatorMakeCredential: function () {
-            return new Promise(function (resolve, reject) {
-                resolve();
-            }); // stub
+            return new Promise(function (resolve, reject) { // stub
+                resolve(true);
+            });
         },
         authenticatorGetAssertion: function () {
-            return new Promise(function (resolve, reject) {
-                resolve();
-            }); // stub
+            return new Promise(function (resolve, reject) { // stub
+                resolve(true);
+            });
         },
         authenticatorCancel: function () {}
     };
@@ -313,7 +426,18 @@ window.fido = (function () {
     fidoAPI.prototype.removeAllAuthenticators = function () {
         _authenticatorList = [];
     };
+    /*********************************************************************************
+     * Everything below this line is an extension to the specification to make extensions easier to work with
+     *********************************************************************************/
+    fidoAPI.prototype.addExtension = function (extensionHook) {
+        this._extensionHookList.push(extensionHook);
+    };
 
+    fidoAPI.prototype.removeExtension = function (extensionHook) {
+        var index = this._extensionHookList.indexOf(extensionHook);
+        if (index === -1) return;
+        this._extensionHookList = this._extensionHookList.splice(index, 1);
+    };
     /*********************************************************************************
      * Everything below this line is an extension to the specification for managing extensions
      *********************************************************************************/
